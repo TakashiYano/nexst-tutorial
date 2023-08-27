5bad53fcd0e64f622c06a148d3c3155ede977cc0
yanot <0tp6324w3261z0v@gmail.com>
2023-08-27 21:38:26 +0900

    ⬆️ Next.js を最新バージョンにアップデート & app ディレクトリを有効化

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
+  experimental: { appDir: true },
   i18n: { locales: ["ja"], defaultLocale: "ja" },
   poweredByHeader: false,
   reactStrictMode: true,
